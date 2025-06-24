b0d7212a31c0e38de7d5905e42e1c2d459ab7296
duvaldyl <dylspatel@gmail.com>
2025-06-23 20:38:13 -0400

    added functionality to upload custom file

--- blueprintparser/main.py
+++ blueprintparser/main.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import os
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def main():
+    return render_template('index.html')
+
+@bp.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        os.makedirs('blueprintparser/static/uploads', exist_ok=True)
+        file = request.files['pdf']
+        file.save('blueprintparser/static/uploads/blueprint.pdf')
+
     return render_template('index.html')
